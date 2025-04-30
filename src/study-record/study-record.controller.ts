7b32e84a94d1bc84fbd4c723164ce62d54fdfb3f
Gustavo <gstvrrds@gmail.com>
2025-04-30 13:43:14 -0300

    feat:adding auth guards in routes

--- src/study-record/study-record.controller.ts
+++ src/study-record/study-record.controller.ts
@@ -1,15 +1,18 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
 import { StudyRecordService } from './study-record.service';
 import { PostStudyRecordDto } from './Dto/study-record.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('study-record')
 export class StudyRecordController {
   constructor(private studyRecordService: StudyRecordService) {}
+  @UseGuards(AuthGuard)
   @Get('/:subjectId')
   async getAllRecords(@Param('subjectId') subjectId: string) {
     return await this.studyRecordService.getAllRecords(subjectId);
   }
 
+  @UseGuards(AuthGuard)
   @Post('/:subjectId')
   async postStudyRecord(
     @Body() record: PostStudyRecordDto,
