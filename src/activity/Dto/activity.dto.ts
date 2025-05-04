cfc077aa657979046cfb3ca0ae9af08fde71b891
Gustavo <gstvrrds@gmail.com>
2025-05-04 19:03:37 -0300

    feat:adjusting activity route

--- src/activity/Dto/activity.dto.ts
+++ src/activity/Dto/activity.dto.ts
@@ -2,7 +2,6 @@ import { createZodDto } from 'nestjs-zod';
 import { z } from 'zod';
 
 export const ActivitySchema = z.object({
-  subjectId: z.string().uuid(),
   name: z.string(),
   description: z.string(),
   percentageConclud: z.number().max(100),
